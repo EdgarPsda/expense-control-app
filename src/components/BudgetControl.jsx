b29bc612fcf59e6b34884db130438a08029fe3f0
EdgarPsda <edgar.psda@gmail.com>
2024-01-23 16:08:44 -0800

    Spent Graph
    Added spent graph, included elements and styles.

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import "react-circular-progressbar/dist/styles.css";
+
 
 const BudgetControl = ({ budget, expenses }) => {
 
     const [available, setAvailable] = useState(0);
     const [spent, setSpent] = useState(0);
+    const [percentage, setPercentage] = useState(0);
 
     useEffect(() => {
         const totalSpent = expenses.reduce((total, expense) => expense.amount + total, 0);
         const totalAvailable = budget - totalSpent;
 
+        // Calc percentage spent
+        const newPercentage = (((budget - totalAvailable) / budget) * 100).toFixed(2);
+
         setSpent(totalSpent);
         setAvailable(totalAvailable);
+        setTimeout(() => {
+            setPercentage(newPercentage);
+        }, 1500);
     }, [expenses])
 
     const budgetFormat = (cant) => {
@@ -22,7 +32,17 @@ const BudgetControl = ({ budget, expenses }) => {
 
     return (
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-            <div>Graph here</div>
+            <div>
+                <CircularProgressbar
+                    value={percentage}
+                    styles={buildStyles({
+                        pathColor: '#3B82F6',
+                        trailColor: '#F5F5F5',
+                        textColor: '#3B82F6'
+                    })}
+                    text={`${percentage}% Spent`}
+                ></CircularProgressbar>
+            </div>
             <div className="contenido-presupuesto">
                 <p>
                     <span>Budget: </span> {budgetFormat(budget)}
